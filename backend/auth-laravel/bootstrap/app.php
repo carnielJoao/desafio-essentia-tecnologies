ab331a8a172029086068ef54635648214524b67a
carnielJoao <joaovictorcarniel.cert.trabalho@gmail.com>
2025-10-16 16:02:16 -0300

    chore(docker): dockerizar frontend e laravel

--- backend/auth-laravel/bootstrap/app.php
+++ backend/auth-laravel/bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,6 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        // Aplica CORS globalmente
+        $middleware->append(HandleCors::class);
+
+        // Seus aliases
         $middleware->alias([
             'jwt' => \App\Http\Middleware\JwtMiddleware::class,
         ]);
--- backend/auth-laravel/config/cors.php
+++ backend/auth-laravel/config/cors.php
@@ -3,10 +3,10 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:4200', 'http://127.0.0.1:4200'],
+    'allowed_origins' => ['http://localhost:8080', 'http://127.0.0.1:8080'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
-    'max_age' => 0,
+    'max_age' => 86400,
     'supports_credentials' => false,
 ];
